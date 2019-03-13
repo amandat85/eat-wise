1b7433342d59d6c3f12b803520cedd7e9861ae8e
amandat <amandatome@me.com>
2019-03-12 23:06:36 -0400

    Code Validation

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -28,167 +28,167 @@ var firebaseController = (function () {
 	};
 })();
 
-// no code will actually be run in this file. that will only happen in the main controller file
+// // no code will actually be run in this file. that will only happen in the main controller file
 
-$(document).ready(function () { 
-// These UI elements will eventually be moved to UI.js
-//GET ELEMENTS IN HTML======================================================
-// var btnSignUp = document.querySelector();
-// var txtLogin = document.querySelector("#username");
-// var btnLogin = document.querySelector("#btnLogin");
-// var txtEmail = document.querySelector("#email");
-// var txtPass = document.querySelector("#password");
-// var passLogin = document.querySelector("#loginPassword");
+// $(document).ready(function () { 
+// // These UI elements will eventually be moved to UI.js
+// //GET ELEMENTS IN HTML======================================================
+// // var btnSignUp = document.querySelector();
+// // var txtLogin = document.querySelector("#username");
+// // var btnLogin = document.querySelector("#btnLogin");
+// // var txtEmail = document.querySelector("#email");
+// // var txtPass = document.querySelector("#password");
+// // var passLogin = document.querySelector("#loginPassword");
 
-// // Event listeners will be moved to main process js file eventually 
-// // and the function for what happend when an event is triggered will be kept in this file
-// //TODO: VALIDATE FIELDS & ADD EXTRA FIELDS TO VARIABLES
-// //LOGIN BUTTON EVENT==============================================
-// btnLogin.addEventListener("click", function(event) {
-//     //get email and password
-//    var login = txtLogin.value;
-//    console.log(email);
-//     var loginPass = passLogin.value;
-//     console.log(loginPass);
-//     console.log(uEmail);
-// firebase.auth().signInWithEmailAndPassword(login, loginPass).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     console.log(errorCode);
-//     console.log(errorMessage);
-//   });
-// });
+// // // Event listeners will be moved to main process js file eventually 
+// // // and the function for what happend when an event is triggered will be kept in this file
+// // //TODO: VALIDATE FIELDS & ADD EXTRA FIELDS TO VARIABLES
+// // //LOGIN BUTTON EVENT==============================================
+// // btnLogin.addEventListener("click", function(event) {
+// //     //get email and password
+// //    var login = txtLogin.value;
+// //    console.log(email);
+// //     var loginPass = passLogin.value;
+// //     console.log(loginPass);
+// //     console.log(uEmail);
+// // firebase.auth().signInWithEmailAndPassword(login, loginPass).catch(function(error) {
+// //     // Handle Errors here.
+// //     var errorCode = error.code;
+// //     var errorMessage = error.message;
+// //     console.log(errorCode);
+// //     console.log(errorMessage);
+// //   });
+// // });
   
-// //SIGN UP BUTTON EVENT=============================================
-// btnSignUp.addEventListener("click", function(event) {
-//     //get email and password
-//    var email = txtEmail.value;
-//    console.log(email);
-//     var pass = txtPass.value;
-//     console.log(pass);
-//     //sign in 
-//    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-//         // Handle Errors here.
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//         // ...
-//         console.log(errorCode);
-//         console.log(errorMessage);
-//       });
-// });
+// // //SIGN UP BUTTON EVENT=============================================
+// // btnSignUp.addEventListener("click", function(event) {
+// //     //get email and password
+// //    var email = txtEmail.value;
+// //    console.log(email);
+// //     var pass = txtPass.value;
+// //     console.log(pass);
+// //     //sign in 
+// //    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+// //         // Handle Errors here.
+// //         var errorCode = error.code;
+// //         var errorMessage = error.message;
+// //         // ...
+// //         console.log(errorCode);
+// //         console.log(errorMessage);
+// //       });
+// // });
 
-// //ADD REALTIME LISTENER TO CHANGED STATE=========================== Recommended
-// firebase.auth().onAuthStateChanged(function(user) {
+// // //ADD REALTIME LISTENER TO CHANGED STATE=========================== Recommended
+// // firebase.auth().onAuthStateChanged(function(user) {
   
-//     if (user) {
-//       // User is signed in.
-//       uEmail = user.email;
-//        uid= user.uid;
-//     } else {
-//       uEmail = "";
-//       uid = "";
+// //     if (user) {
+// //       // User is signed in.
+// //       uEmail = user.email;
+// //        uid= user.uid;
+// //     } else {
+// //       uEmail = "";
+// //       uid = "";
       
-//     }
-//     console.log(uEmail, uid); //Tested - has user id and email
+// //     }
+// //     console.log(uEmail, uid); //Tested - has user id and email
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-	UIController.getDOMStrings
+// 	UIController.getDOMStrings
 
-	// These UI elements will eventually be moved to UI.js
-	//GET ELEMENTS IN HTML======================================================
-	// var btnSignUp = document.querySelector();
-	// var txtLogin = document.querySelector("#username");
-	// var btnLogin = document.querySelector("#btnLogin");
-	// var txtEmail = document.querySelector("#email");
-	// var txtPass = document.querySelector("#password");
-	// var passLogin = document.querySelector("#loginPassword");
+// 	// These UI elements will eventually be moved to UI.js
+// 	//GET ELEMENTS IN HTML======================================================
+// 	// var btnSignUp = document.querySelector();
+// 	// var txtLogin = document.querySelector("#username");
+// 	// var btnLogin = document.querySelector("#btnLogin");
+// 	// var txtEmail = document.querySelector("#email");
+// 	// var txtPass = document.querySelector("#password");
+// 	// var passLogin = document.querySelector("#loginPassword");
 
-	// // Event listeners will be moved to main process js file eventually 
-	// // and the function for what happend when an event is triggered will be kept in this file
-	// //TODO: VALIDATE FIELDS & ADD EXTRA FIELDS TO VARIABLES
-	// //LOGIN BUTTON EVENT==============================================
-	// btnLogin.addEventListener("click", function(event) {
-	//     //get email and password
-	//    var login = txtLogin.value;
-	//    console.log(email);
-	//     var loginPass = passLogin.value;
-	//     console.log(loginPass);
-	//     console.log(uEmail);
-	// firebase.auth().signInWithEmailAndPassword(login, loginPass).catch(function(error) {
-	//     // Handle Errors here.
-	//     var errorCode = error.code;
-	//     var errorMessage = error.message;
-	//     console.log(errorCode);
-	//     console.log(errorMessage);
-	//   });
-	// });
+// 	// // Event listeners will be moved to main process js file eventually 
+// 	// // and the function for what happend when an event is triggered will be kept in this file
+// 	// //TODO: VALIDATE FIELDS & ADD EXTRA FIELDS TO VARIABLES
+// 	// //LOGIN BUTTON EVENT==============================================
+// 	// btnLogin.addEventListener("click", function(event) {
+// 	//     //get email and password
+// 	//    var login = txtLogin.value;
+// 	//    console.log(email);
+// 	//     var loginPass = passLogin.value;
+// 	//     console.log(loginPass);
+// 	//     console.log(uEmail);
+// 	// firebase.auth().signInWithEmailAndPassword(login, loginPass).catch(function(error) {
+// 	//     // Handle Errors here.
+// 	//     var errorCode = error.code;
+// 	//     var errorMessage = error.message;
+// 	//     console.log(errorCode);
+// 	//     console.log(errorMessage);
+// 	//   });
+// 	// });
 
-	// //SIGN UP BUTTON EVENT=============================================
-	// btnSignUp.addEventListener("click", function(event) {
-	//     //get email and password
-	//    var email = txtEmail.value;
-	//    console.log(email);
-	//     var pass = txtPass.value;
-	//     console.log(pass);
-	//     //sign in 
-	//    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-	//         // Handle Errors here.
-	//         var errorCode = error.code;
-	//         var errorMessage = error.message;
-	//         // ...
-	//         console.log(errorCode);
-	//         console.log(errorMessage);
-	//       });
-	// });
-	//set value in local storage = true
-	//set value in local storage= false on logout
+// 	// //SIGN UP BUTTON EVENT=============================================
+// 	// btnSignUp.addEventListener("click", function(event) {
+// 	//     //get email and password
+// 	//    var email = txtEmail.value;
+// 	//    console.log(email);
+// 	//     var pass = txtPass.value;
+// 	//     console.log(pass);
+// 	//     //sign in 
+// 	//    firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+// 	//         // Handle Errors here.
+// 	//         var errorCode = error.code;
+// 	//         var errorMessage = error.message;
+// 	//         // ...
+// 	//         console.log(errorCode);
+// 	//         console.log(errorMessage);
+// 	//       });
+// 	// });
+// 	//set value in local storage = true
+// 	//set value in local storage= false on logout
 
-	// //ADD REALTIME LISTENER TO CHANGED STATE=========================== Recommended
-	// firebase.auth().onAuthStateChanged(function(user) {
+// 	// //ADD REALTIME LISTENER TO CHANGED STATE=========================== Recommended
+// 	// firebase.auth().onAuthStateChanged(function(user) {
 
-	//     if (user) {
-	//       // User is signed in.
-	//       uEmail = user.email;
-	//        uid= user.uid;
-	//     } else {
-	//       uEmail = "";
-	//       uid = "";
+// 	//     if (user) {
+// 	//       // User is signed in.
+// 	//       uEmail = user.email;
+// 	//        uid= user.uid;
+// 	//     } else {
+// 	//       uEmail = "";
+// 	//       uid = "";
 
-	//     }
-	//     console.log(uEmail, uid); //Tested - has user id and email
+// 	//     }
+// 	//     console.log(uEmail, uid); //Tested - has user id and email
 
-});
-//Test not defined - scoping issue?? - Do you need a snapshot of the onAuthStateChanged?
-// var user = firebase.auth().currentUser;
-
-// if (user) {
-//   // User is signed in.
-// } else {
-//   // No user is signed in.
-// }
-// var user = firebase.auth().currentUser;
-// var email, uid;
-// if (user != null) {
-//   email = user.email;
-//   uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
-//                    // this value to authenticate with your backend server, if
-//                    // you have one. Use User.getToken() instead.
-// }
-
-//   function writeUserData(favRecipes, favRestaurants) {
-//     firebase.database().ref('users/' + uid).set({
-//       // userEmail: uEmail,
-//       // userID: uid,
-//       favRecipes: favRecipes,
-//       favRestaurants: favRestaurants,
-//     });
-//   }
-//   console.log(writeUserData);
-//   console.log(userEmail)
-//  //Not pushing anything to the database. userEmail and userID undefined
 // });
+// //Test not defined - scoping issue?? - Do you need a snapshot of the onAuthStateChanged?
+// // var user = firebase.auth().currentUser;
+
+// // if (user) {
+// //   // User is signed in.
+// // } else {
+// //   // No user is signed in.
+// // }
+// // var user = firebase.auth().currentUser;
+// // var email, uid;
+// // if (user != null) {
+// //   email = user.email;
+// //   uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+// //                    // this value to authenticate with your backend server, if
+// //                    // you have one. Use User.getToken() instead.
+// // }
+
+// //   function writeUserData(favRecipes, favRestaurants) {
+// //     firebase.database().ref('users/' + uid).set({
+// //       // userEmail: uEmail,
+// //       // userID: uid,
+// //       favRecipes: favRecipes,
+// //       favRestaurants: favRestaurants,
+// //     });
+// //   }
+// //   console.log(writeUserData);
+// //   console.log(userEmail)
+// //  //Not pushing anything to the database. userEmail and userID undefined
+// // });
 var database =  firebase.database(); 
 document.querySelector("#search").addEventListener("click", function (event) {
 	event.preventDefault();
@@ -240,7 +240,12 @@ document.querySelector("#search").addEventListener("click", function (event) {
 		// return true;//Everything is good. go to next page.
 		var mealtime = $("input:radio[name='mealtime']:checked").val();
 		console.log(mealtime);
-		var intolerance = $("input:checkbox[name='intolerance']:checked").val();
+		// var intolerance = $("input:checkbox[name='intolerance']:checked").val();
+		//json. stringfy and parse array to firebase
+		var intolerance = [];
+            $('input[name="intolerance"]:checked').each(function () {
+                intolerance.push(this.value);
+            });
 		console.log(intolerance);
 		var diet= $("input:radio[name='diet']:checked").val();
 		console.log(diet);
@@ -267,8 +272,5 @@ document.querySelector("#search").addEventListener("click", function (event) {
 		database.ref("users/").push(searchResults);
 		console.log(searchResults);
 	}
-
 	//snapshot needed if authentication
-
 });
-
